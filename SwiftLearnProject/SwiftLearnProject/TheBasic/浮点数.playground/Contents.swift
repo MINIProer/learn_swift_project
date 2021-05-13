93b56cdf86abf231f0fc95fcc9a9bec2d56e402e
jiangshulun <jiangshulun@joyme.sg>
2021-05-13 14:54:14 +0800

    Feat：基础部分 - <浮点数>

---
+++ SwiftLearnProject/SwiftLearnProject/TheBasic/浮点数.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+/* ------------------------------< 浮点数 >------------------------------ */
+
+let double_cl : Double = 3.14159265758123123
+let float_cl : Float = 3.14159265758
+
+/*
+    Double表示64位浮点数
+    Float表示32位浮点数
+ 
+    Double的精度要比Float的精度高
+ 
+    两种类型都匹配的情况下，优先选择Double
+ */
+
+// ❌ SwiftGG官网上对Double的解释好像有问题，double_cl至少有16位，官网说是15位
